5bb95ca1c9da9d00257fe740e1e3074127e77f27
diki_ <diki_@ILIEVPC>
2020-04-26 17:37:03 +0200

    Add class library projects for Unit & Integration tests.

---
+++ FlixOne.BookStore.ProductService.IntegrationTests/Services/ProductTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Xunit;
+
+namespace FlixOne.BookStore.ProductService.IntegrationTests.Services
+{
+    public class ProductTest
+    {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+        public ProductTest()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = _server.CreateClient();
+            _client.BaseAddress = new Uri("http://localhost:57910");
+        }
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            //Act
+            var response = await _client.GetAsync("/GetProduct");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            //Assert
+            Assert.NotEmpty(responseString);
+        }
+    }
+}
---
+++ FlixOne.BookStore.ProductService.UnitTests/Services/ProductTests.cs
@@ -0,0 +1,63 @@
+﻿using FlixOne.BookStore.ProductService.Controllers;
+using FlixOne.BookStore.ProductService.Helpers.Extensions;
+using FlixOne.BookStore.ProductService.Models;
+using FlixOne.BookStore.ProductService.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace FlixOne.BookStore.ProductService.UnitTests.Services
+{
+    public class ProductTests
+    {
+        private IEnumerable<ProductViewModel> GetProducts()
+        {
+            var productVm = new List<ProductViewModel>
+            {
+                new ProductViewModel
+                {
+                    CategoryId = Guid.NewGuid(),
+                    CategoryDescription = "Category Description",
+                    CategoryName = "Category Name",
+                    ProductDescription = "Product Description",
+                    ProductId = Guid.NewGuid(),
+                    ProductImage = "Product Image",
+                    ProductName = "Product Name",
+                    ProductPrice = 112M
+                },
+                new ProductViewModel
+                {
+                    CategoryId = Guid.NewGuid(),
+                    CategoryDescription = "Category Description 1",
+                    CategoryName = "Category Name 1",
+                    ProductDescription = "Product Description 1",
+                    ProductId = Guid.NewGuid(),
+                    ProductImage = "Product Image 1",
+                    ProductName = "Product Name 1",
+                    ProductPrice = 12M
+                }
+            };
+
+            return productVm;
+        }
+
+        [Fact]
+        public void Get_Returns_ActionResults()
+        {
+            //Arrange
+            var mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAll().ToViewModel()).Returns(GetProducts());
+            var controller = new ProductController(mockRepo.Object);
+            //Act
+            var result = controller.Get();
+            //Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<ProductViewModel>>(viewResult.Value);
+            Assert.Equal(2, model.Count());
+        }
+    }
+}
